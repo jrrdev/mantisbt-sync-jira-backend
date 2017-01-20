4c871616c7a01fa98e7b48b60188084b186f36f7
jrrdev <devarulg@hotmail.fr>
2017-01-20 13:28:41 +0000

    Removes some debug logging

--- server.js
+++ server.js
@@ -35,14 +35,11 @@ if (config.target.username) {
 
 function createSubTasks(parentKey) {
 
-	console.log("Start create subtasks" + JSON.stringify(config.subTasks));
-
     if (config.subTasks && parentKey) {
 		logger.info("Creating subtask for Jira issue %s", parentKey);
 	
 		for (var i = 0; i < config.subTasks.length; i++) {
 			var subtask = config.subTasks[i];
-			console.log("Start create subtask" + JSON.stringify(subtask));
             var jiraSubtask = {
                 "fields": {
                     "project": {
@@ -60,21 +57,16 @@ function createSubTasks(parentKey) {
                 }
             };
 			
-			console.log("POST create subtask");
 			logger.info("Creating subtask for Jira issue %s - %s", parentKey, subtask.summary);
-			console.log("POST 2 create subtask");
 			
             jiraClient.post('/jira2/rest/api/2/issue', jiraSubtask, function (err, req, res, obj) {
                 if (err) {
                     logger.error(err);
                 }
             });
-			
-			console.log("End POST create subtask");
         }
     }
 	
-	console.log("End create subtask");
 	logger.info("End creating subtask for Jira issue %s", parentKey);
 }
 
